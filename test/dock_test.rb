7e541cae274667ff9ea741ac1d0a6b056c739036
Jacob Volpe <volpe.jacob@gmail.com>
2020-12-31 10:34:01 -0700

    First dock tests

--- test/dock_test.rb
+++ test/dock_test.rb
@@ -6,5 +6,16 @@ require './lib/dock'
 require 'pry'
 
 class DockTest < Minitest::Test 
-  
+  def test_dock_exisits 
+    dock = Dock.new("The Rowing Dock", 3) 
+
+    assert_instance_of Dock, dock
+  end
+
+  def test_dock_has_readable_attributes
+    dock = Dock.new("The Rowing Dock", 3) 
+
+    assert_equal "The Rowing Dock", dock.name
+    assert_equal 3, dock.max_rental_time
+  end
 end
