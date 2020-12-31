8eda47c34690e08780eff5fdb88954ca6f192415
Jacob Volpe <volpe.jacob@gmail.com>
2020-12-31 11:34:28 -0700

    Wrap up iteration 3

--- lib/dock.rb
+++ lib/dock.rb
@@ -12,4 +12,23 @@ class Dock
   def rent(boat, renter)
     @rental_log[boat] = renter
   end
+
+  def find_total_charge(boat)
+    hours_rented = boat.hours_rented
+    pph = boat.price_per_hour
+    max = @max_rental_time
+    actual = [hours_rented, max].min
+    charge = actual * pph
+  end
+
+
+  def charge(boat)
+    holder = {}
+    holder[:card_nmuber] = @rental_log[boat].credit_card_number
+    holder[:amount] = find_total_charge(boat)
+    holder
+  end
+
+
 end
+
--- test/dock_test.rb
+++ test/dock_test.rb
@@ -47,4 +47,51 @@ class DockTest < Minitest::Test
     assert_equal eugene, dock.rental_log[sup_1]
     assert_equal [kayak_1, kayak_2, sup_1], dock.rental_log.keys
   end
+
+  def test_find_total_charge
+    dock = Dock.new("The Rowing Dock", 3) 
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242") 
+    dock.rent(kayak_1, patrick)
+    kayak_1.add_hour
+    kayak_1.add_hour
+    expected = dock.find_total_charge(kayak_1)
+
+    assert_equal 40, expected
+  end
+
+  def test_dock_charge
+    dock = Dock.new("The Rowing Dock", 3) 
+    kayak_1 = Boat.new(:kayak, 20)
+    patrick = Renter.new("Patrick Star", "4242424242424242") 
+    dock.rent(kayak_1, patrick)
+    kayak_1.add_hour
+    kayak_1.add_hour
+    expected = dock.charge(kayak_1)
+
+    assert_equal 2, kayak_1.hours_rented
+    assert_equal 2, expected.count
+    assert_equal "4242424242424242", expected[:card_nmuber]
+    assert_equal 40, expected[:amount]
+  end
+
+  def test_dock_charge_wont_go_above_max
+    dock = Dock.new("The Rowing Dock", 3) 
+    sup_1 = Boat.new(:standup_paddle_board, 15)
+    eugene = Renter.new("Eugene Crabs", "1313131313131313") 
+    dock.rent(sup_1, eugene)
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    sup_1.add_hour
+    expected = dock.charge(sup_1)
+
+    assert_equal 5, sup_1.hours_rented
+    assert_equal 2, expected.count
+    assert_equal "1313131313131313", expected[:card_nmuber]
+    assert_equal 45, expected[:amount]
+  end
+
+
 end
